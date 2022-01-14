b4e39fea9b5e2077f11671f667e7c3d473a17fd1
knawattranakul <knawattranakul@splunk.com>
2022-01-13 17:22:36 -0800

    fix SemGrep issues

--- smtp_consts.py
+++ smtp_consts.py
@@ -61,3 +61,5 @@ ERR_CODE_UNAVAILABLE = "Error code unavailable"
 ERR_MSG_UNAVAILABLE = "Error message unavailable. Please check the asset configuration and|or action parameters"
 TYPE_ERR_MSG = "Error occurred while connecting to the server. Please check the asset configuration and|or the action parameters"
 PARSE_ERR_MSG = "Unable to parse the error message. Please check the asset configuration and|or action parameters"
+
+DEFAULT_TIMEOUT = 30  # 30 seconds
